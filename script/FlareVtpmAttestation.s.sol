5ad77565565e641eeb65bda36ef2af9a5a0f9008
Olivier Fuxet <olivier.fuxet@gmail.com>
2025-07-23 14:23:43 +0200

    Fix deployment script using foundry-upgrades

--- script/FlareVtpmAttestation.s.sol
+++ script/FlareVtpmAttestation.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 
 import {FlareVtpmAttestation} from "../contracts/FlareVtpmAttestation.sol";
 import {OidcSignatureVerification} from "../contracts/verifiers/OidcSignatureVerification.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Upgrades} from "@openzeppelin/openzeppelin-foundry-upgrades/Upgrades.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract FlareVtpmAttestationScript is Script {
@@ -14,36 +14,18 @@ contract FlareVtpmAttestationScript is Script {
     string iss = vm.envString("ISS");
     bool secboot = vm.envBool("SECBOOT");
 
-    FlareVtpmAttestation flareVtpmImplementation;
-    FlareVtpmAttestation flareVtpm;
-    OidcSignatureVerification oidcVerifierImplementation;
-    OidcSignatureVerification oidcVerifier;
-
     function deploy() public {
         // Starting the broadcast of transactions from the deployer account
         vm.startBroadcast(deployerPrivateKey);
 
         address deployer = vm.addr(deployerPrivateKey);
 
-        // Deploy the FlareVtpmAttestation implementation
-        flareVtpmImplementation = new FlareVtpmAttestation();
-        console.log("FlareVtpmAttestation implementation deployed at:", address(flareVtpmImplementation));
-
-        // Prepare initialization data for FlareVtpmAttestation
-        bytes memory flareVtpmInitData = abi.encodeWithSelector(
-            FlareVtpmAttestation.initialize.selector,
-            deployer,
-            hwmodel,
-            swname,
-            imageDigest,
-            iss,
-            secboot
-        );
-
         // Deploy the FlareVtpmAttestation proxy
-        ERC1967Proxy flareVtpmProxy = new ERC1967Proxy(address(flareVtpmImplementation), flareVtpmInitData);
-        flareVtpm = FlareVtpmAttestation(address(flareVtpmProxy));
-        console.log("FlareVtpmAttestation proxy deployed at:", address(flareVtpm));
+        address flareVtpm = Upgrades.deployUUPSProxy(
+            "FlareVtpmAttestation.sol",
+            abi.encodeCall(FlareVtpmAttestation.initialize, (deployer, hwmodel, swname, imageDigest, iss, secboot))
+        );
+        console.log("FlareVtpmAttestation proxy deployed at:", flareVtpm);
 
         // Log that the base configuration has been set during initialization
         console.log("Base quote configuration set with:");
@@ -53,23 +35,15 @@ contract FlareVtpmAttestationScript is Script {
         console.log("  Issuer:", iss);
         console.log("  Secure Boot:", secboot);
 
-        // Deploy the OidcSignatureVerification implementation
-        oidcVerifierImplementation = new OidcSignatureVerification();
-        console.log("OidcSignatureVerification implementation deployed at:", address(oidcVerifierImplementation));
-
-        // Prepare initialization data for OidcSignatureVerification
-        bytes memory oidcInitData = abi.encodeWithSelector(
-            OidcSignatureVerification.initialize.selector,
-            deployer
-        );
-
         // Deploy the OidcSignatureVerification proxy
-        ERC1967Proxy oidcVerifierProxy = new ERC1967Proxy(address(oidcVerifierImplementation), oidcInitData);
-        oidcVerifier = OidcSignatureVerification(address(oidcVerifierProxy));
-        console.log("OidcSignatureVerification proxy deployed at:", address(oidcVerifier));
+        address oidcVerifier = Upgrades.deployUUPSProxy(
+            "OidcSignatureVerification.sol",
+            abi.encodeCall(OidcSignatureVerification.initialize, (deployer))
+        );
+        console.log("OidcSignatureVerification proxy deployed at:", oidcVerifier);
 
         // Set the token type verifier to the OidcSignatureVerification contract
-        flareVtpm.setTokenTypeVerifier(address(oidcVerifier));
+        FlareVtpmAttestation(flareVtpm).setTokenTypeVerifier(oidcVerifier);
         console.log("FlareVtpmAttestation token type verifier set to OidcSignatureVerification");
 
         vm.stopBroadcast();
